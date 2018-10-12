46962b1e04944557c565c9f07c27dcf0f35cbdee
aaronamm <theeranitp@gmail.com>
2018-10-12 17:28:50 +0700

    initial setup module

--- Export-SqlQuery.ps1
+++ Export-SqlQuery.ps1
@@ -11,54 +11,6 @@ $assemblyPath = Join-Path -Path $outputDir -ChildPath "$libraryName.dll"
 #LoadFrom() look for the depepent DLLs in the same directory
 $assembly = [Reflection.Assembly]::LoadFrom($assemblyPath)   
 
-function Export-SqlQuery {
-    param(
-        [Parameter(Mandatory = $True)] [string] $ConnectionString, 
-        [Parameter(Mandatory = $True)]
-        [CodeSanook.SqlGenerator.DatabaseType] $DatabaseType, 
-        [Parameter(Mandatory = $True)] [string] $Query, 
-        [Parameter(Mandatory = $True)] [string] $Template,
-        [Parameter(Mandatory = $True)] [string] $FilePath
-    )
-
-    #prepare parameters object
-    $options = New-Object CodeSanook.SqlGenerator.ExportOptions
-    $options.DatabaseType = $DatabaseType 
-    $options.ConnectionString = $ConnectionString 
-    $options.Query = $Query
-    $options.Template = $Template
-
-    $fileStream = New-Object `
-        -TypeName System.IO.FileStream `
-        -ArgumentList @($FilePath, [System.IO.FileMode]::Append, [System.IO.FileAccess]::Write)
-    $options.Stream = $fileStream 
-
-    # export SQL query and pipe to a file
-    $tool = New-Object CodeSanook.SqlGenerator.SqlExportTool
-    $tool.Export($options)
-    $fileStream.Close()
-}
-
-function Get-ConnectionString{
-    param(
-        [Parameter(Mandatory = $True)] [string] $Server,
-        [Parameter(Mandatory = $True)] [string] $Database
-    )
-
-    if ($Password) {
-        #decrypt input password
-        $bstr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Password)
-        $databaseUserPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($bstr)
-        $connectionString = "Server=$Server;Database=$Database;User Id=sa; Password=$databaseUserPassword;"
-    }
-    else {
-        #Trusted Connection Windows user login
-        $connectionString = "Server=$Server;Database=$Database;Trusted_Connection=True";
-    }
-
-    $connectionString
-}
-
 #*********************Begin of using a script*********************
 
 $database = "testdb"
