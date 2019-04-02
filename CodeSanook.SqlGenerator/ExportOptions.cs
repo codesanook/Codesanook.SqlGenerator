237173a34122a8ef815e26bed923a01ef35d6e99
aaronamm <theeranitp@gmail.com>
2019-04-02 16:00:07 +0700

    More comment in ExportOptions, simplify Export-SqlQuery.ps1

--- CodeSanook.SqlGenerator/ExportOptions.cs
+++ CodeSanook.SqlGenerator/ExportOptions.cs
@@ -13,7 +13,14 @@ namespace CodeSanook.SqlGenerator
         public DatabaseType DatabaseType { get; set; }
         public string ConnectionString { get; set; }
         public string Query { get; set; }
+
+        // Built-in placeholders are: 
+        // #{columnName} for a value of a given column name from a select statement
+        // #{!'columnName} for a value of a given column name from a select statement without wrap quote
+        // #{col*} for CSV of all values in a row
+        // #{col*} for CSV of all column names in a row
         public string Template { get; set; }
+
         public Stream Stream { get; set; }
     }
 }
--- Export-SqlQuery.ps1
+++ Export-SqlQuery.ps1
@@ -1,58 +1,26 @@
-﻿<#
-username and password parameters are optional.
-If you run in the same machine of SQL server, you can ignore these parameter 
-and use Windows authentication
-#>
-param(
-	[Parameter(Mandatory = $False)] [string] $Username,
-	[Parameter(Mandatory = $False)] [string] $Password
-)
-
+﻿
 Import-Module -Name "./Export-SqlQueryModule" -Verbose -Force
 #*********************Begin of using a script*********************
 
-$database = "testdb"
 $server = ".\"
-
-$connectionString = Get-ConnectionString -Server $server -Database $database -Username $Username -Password $Password
+$database = "JetabroadOperation"
+$connectionString = Get-ConnectionString -Server $server -Database $database
 $databaseType = [CodeSanook.SqlGenerator.DatabaseType]::SqlServer
 
 $fileOutputPath = Join-Path -Path $PSScriptRoot -ChildPath "output-script.sql" 
 Remove-Item -Path $fileOutputPath -Force -ErrorAction Ignore
 
-#Get Users items
 $query = @"
-    SELECT * FROM Users
+    SELECT * FROM SabreAlternateOfficeAirlinePreference
+    WHERE SabreParameterAlternateOfficeFk = '869981F6-AD70-48A0-9CC5-630B4DD218B1'
 "@
 
-# Built-in placeholders are: 
-# #{columnName} for a value of a given column name from a select statement
-# #{!'columnName} for a value of a given column name from a select statement without wrap quote
-# #{col*} for CSV of all values in a row
-# ##{col*} for CSV of all column names in a row
-
-# Get all columns
 $template = @"
-    INSERT INTO [Users]
+    INSERT INTO [SabreAlternateOfficeAirlinePreference]
         (##{col*}) 
     VALUES 
         (#{col*})
 
-"@
-
-Export-SqlQuery `
-	-ConnectionString $connectionString `
-	-DatabaseType $databaseType `
-	-Query $query `
-	-Template $template `
-	-FilePath $fileOutputPath
-
-# Get specific columns
-$template = @"
-    INSERT INTO [Users]
-        (Id, FullName) 
-    VALUES 
-        (#{Id}, '#{!'FirstName} #{!'LastName}')
 "@
 
 Export-SqlQuery `
