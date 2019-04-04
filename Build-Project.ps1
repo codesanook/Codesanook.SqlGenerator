d03dfb64c1a4837fba481a9b44a217e657cfbaf1
aaronamm <theeranitp@gmail.com>
2019-04-04 21:45:23 +0700

    Improve build project script with vswhere and call operator

--- Build-Project.ps1
+++ Build-Project.ps1
@@ -6,24 +6,16 @@ function Invoke-MsBuild {
 		[Parameter(Mandatory=$True)] [string] $Configuration, 
 		[Parameter(Mandatory=$True)] [string] $Target 
 	)
-
-	$command = `
-		'& "{0}" "{1}" /t:{2} /p:OutputPath="{3}" /p:Configuration={4}'`
-		-f `
-		$ExePath, `
-		$ProjectFile, `
-		$Target, `
-		$OutputPath, `
-		$Configuration
 	
-	"$command"
-	& $command #use call operator
+	& $ExePath "$projectFile" /t:"$Target" /p:OutputPath="$OutputPath" /p:Configuration="$Configuration"
+	#use call operator
 }
 
 #restore nuget
 ./nuget restore "./CodeSanook.SqlGenerator.sln"
-##TODO use VSwhere
-$exePath = "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\amd64\MSBuild.exe"
+
+$buildToolPath = vswhere -Latest -products * -Requires Microsoft.Component.MSBuild -Property InstallationPath
+$exePath = "$buildToolPath\MSBuild\15.0\Bin\MSBuild.exe"
 
 $projectRoot = "./CodeSanook.SqlGenerator"
 $projectName = "CodeSanook.SqlGenerator"
@@ -32,8 +24,10 @@ $projectFile = "$projectRoot/$projectName.csproj"
 $configuration = "Release"
 $outputPath = "bin/$configuration"
 
+# Clean the project
 $target ="clean"
 Invoke-MsBuild -ExePath $exePath -ProjectFile $projectFile -OutputPath $outputPath -Configuration $configuration -Target $target
 
+# Build the project
 $target ="build"
 Invoke-MsBuild -ExePath $exePath -ProjectFile $projectFile -OutputPath $outputPath -Configuration $configuration -Target $target
